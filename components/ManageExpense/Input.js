27c86564c1ae9f8b92ac7a4930d9bd9ab94a30a0
Andyson <andyson@mypatricia.co>
2022-05-18 21:50:02 +0100

    Styling Validation Error

--- components/ManageExpense/Input.js
+++ components/ManageExpense/Input.js
@@ -2,7 +2,7 @@ import { View, Text, TextInput, StyleSheet } from "react-native";
 
 import { GlobalStyles } from "../../constants/styles";
 
-function Input({ label, style, textInputConfig }) {
+function Input({ label, invalid, style, textInputConfig }) {
 
     const inputStyles = [styles.input];
 
@@ -10,9 +10,13 @@ function Input({ label, style, textInputConfig }) {
       inputStyles.push(styles.inputMultiline);  
     }
 
+    if (invalid){
+        inputStyles.push(styles.invalidInput); 
+    }
+
     return (
         <View style={[styles.inputContainer, style]}>
-            <Text style={styles.label}>{label}</Text>
+            <Text style={[styles.label, invalid && styles.invalidLabel ]}>{label}</Text>
             <TextInput style={inputStyles} {...textInputConfig} />
         </View>
     );
@@ -21,25 +25,30 @@ function Input({ label, style, textInputConfig }) {
 export default Input;
 
 const styles = StyleSheet.create({
-    inputContainer: {
-        marginHorizontal: 4,
-        marginVertical: 8,
-    },
-    label:{
-        fontSize: 12,
-        color: GlobalStyles.colors.primary100,
-        marginBottom: 4,
-
-    },
-    input: {
-        backgroundColor: GlobalStyles.colors.primary100,
-        color: GlobalStyles.colors.primary700,
-        padding: 6,
-        borderRadius: 6,
-        fontSize: 18
-    },
-    inputMultiline: {
-        minHeight: 100,
-        textAlignVertical: 'top'
-    }
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 8,
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4,
+  },
+  input: {
+    backgroundColor: GlobalStyles.colors.primary100,
+    color: GlobalStyles.colors.primary700,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18,
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: "top",
+  },
+  invalidLabel: {
+    color: GlobalStyles.colors.error500,
+  },
+  invalidInput: {
+    backgroundColor: GlobalStyles.colors.error50,
+  },
 });
